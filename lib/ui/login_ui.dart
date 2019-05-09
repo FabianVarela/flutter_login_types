0bd35d0e396b2abb243f706cdee8da60aa34b581
Fabian Varela Bonett <fvarela@smarttaxi.co>
2019-05-09 10:58:51 -0500

    Adding controllers dispose, remove single child scroll and add resize to avoid padding

--- lib/ui/login_ui.dart
+++ lib/ui/login_ui.dart
@@ -20,6 +20,9 @@ class _LoginUIState extends State<LoginUI> {
 
   @override
   void dispose() {
+    _textEmailController.dispose();
+    _textPasswordController.dispose();
+
     bloc.dispose();
     super.dispose();
   }
@@ -37,89 +40,90 @@ class _LoginUIState extends State<LoginUI> {
 
           return Scaffold(
             key: _scaffoldKey,
+            resizeToAvoidBottomPadding: true,
             appBar: AppBar(
               title: Text("Login with BLoC"),
               centerTitle: true,
             ),
-            body: SingleChildScrollView(
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    height: MediaQuery.of(context).size.height,
-                    padding: EdgeInsets.all(16),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        StreamBuilder<String>(
-                          stream: bloc.email,
-                          builder: (context, emailSnapshot) => CustomTextField(
-                                label: "Email",
-                                textController: _textEmailController,
-                                hint: "Enter email",
-                                isRequired: true,
-                                requiredMessage: "The email is required",
-                                onChange: bloc.changeEmail,
-                                inputType: TextInputType.emailAddress,
-                                errorText: emailSnapshot.error,
-                              ),
-                        ),
-                        SizedBox(height: 20),
-                        StreamBuilder<String>(
-                          stream: bloc.password,
-                          builder: (context, passwordSnapshot) =>
-                              CustomTextField(
-                                label: "Password",
-                                hint: "Enter password",
-                                isRequired: true,
-                                requiredMessage: "The password is required",
-                                onChange: bloc.changePassword,
-                                errorText: passwordSnapshot.error,
-                                hasPassword: true,
-                                textController: _textPasswordController,
-                              ),
-                        ),
-                        SizedBox(height: 20),
-                        StreamBuilder<bool>(
-                          stream: bloc.isValidData,
-                          builder: (context, snapshot) => Row(
-                                children: <Widget>[
-                                  Expanded(
-                                    flex: 1,
-                                    child: RaisedButton(
-                                      color: Colors.tealAccent,
-                                      child: Text("Submit"),
-                                      onPressed: snapshot.hasData
-                                          ? () => bloc.authenticate()
-                                          : null,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  StreamBuilder<bool>(
-                    stream: bloc.isLoading,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData && snapshot.data) {
-                        return Container(
-                          height: MediaQuery.of(context).size.height,
-                          color: Colors.grey.withOpacity(.7),
-                          child: Center(
-                            child: CircularProgressIndicator(
-                              backgroundColor: Colors.green,
+            body: Stack(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  height: MediaQuery.of(context).size.height,
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    //shrinkWrap: true,
+                    children: <Widget>[
+                      StreamBuilder<String>(
+                        stream: bloc.email,
+                        builder: (context, emailSnapshot) => CustomTextField(
+                              label: "Email",
+                              textController: _textEmailController,
+                              hint: "Enter email",
+                              isRequired: true,
+                              requiredMessage: "The email is required",
+                              onChange: bloc.changeEmail,
+                              inputType: TextInputType.emailAddress,
+                              errorText: emailSnapshot.error,
                             ),
+                      ),
+                      SizedBox(height: 20),
+                      StreamBuilder<String>(
+                        stream: bloc.password,
+                        builder: (context, passwordSnapshot) =>
+                            CustomTextField(
+                              label: "Password",
+                              hint: "Enter password",
+                              isRequired: true,
+                              requiredMessage: "The password is required",
+                              onChange: bloc.changePassword,
+                              errorText: passwordSnapshot.error,
+                              hasPassword: true,
+                              textController: _textPasswordController,
+                            ),
+                      ),
+                      SizedBox(height: 20),
+                      StreamBuilder<bool>(
+                        stream: bloc.isValidData,
+                        builder: (context, snapshot) => Row(
+                              children: <Widget>[
+                                Expanded(
+                                  flex: 1,
+                                  child: RaisedButton(
+                                    color: Colors.tealAccent,
+                                    child: Text("Submit"),
+                                    onPressed: snapshot.hasData
+                                        ? () => bloc.authenticate()
+                                        : null,
+                                  ),
+                                ),
+                              ],
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+                StreamBuilder<bool>(
+                  stream: bloc.isLoading,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData && snapshot.data) {
+                      return Container(
+                        height: MediaQuery.of(context).size.height,
+                        color: Colors.grey.withOpacity(.7),
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            backgroundColor: Colors.green,
                           ),
-                        );
-                      } else {
-                        return Container();
-                      }
-                    },
-                  )
-                ],
-              ),
+                        ),
+                      );
+                    } else {
+                      return Container();
+                    }
+                  },
+                )
+              ],
             ),
           );
         }
