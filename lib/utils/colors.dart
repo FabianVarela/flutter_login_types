2188a1d6e0465c82feb0d489443c7822ade38dcf
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-01-28 12:32:10 -0500

    Create palette colors file

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightGreen = Color(0xFF3ACEB9);
+  static const Color lightRed = Color(0xFFF45728);
+  static const Color lightWhite = Color(0xFFF1F3FC);
+  static const Color darkBlue = Color(0xFF040941);
+  static const Color grey = Color(0xFF161616);
+}
