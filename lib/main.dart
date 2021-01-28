56a4ed87462e28a7f73444ee6b73a99b3f13a402
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-01-27 22:54:01 -0500

    Update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:login_bloc/ui/login_ui.dart';
 
+import 'ui/home_page_ui.dart';
+import 'ui/login_ui.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: LoginUI(),
+      theme: ThemeData(primarySwatch: Colors.teal),
+      initialRoute: '/',
+      routes: {
+        '/': (context) {
+          return LoginUI();
+        },
+        '/home': (context) {
+          return HomePageUI();
+        },
+      },
     );
   }
 }
