40aa2dc0ea287d8669d4bc4003d2343d2955f416
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-01-27 22:53:49 -0500

    Update LoginPageUI screen

--- lib/ui/login_ui.dart
+++ lib/ui/login_ui.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_bloc/bloc/login_bloc.dart';
 import 'package:login_bloc/ui/custom_textfield.dart';
-import 'package:login_bloc/ui/home_page_ui.dart';
 
 class LoginUI extends StatefulWidget {
   @override
@@ -9,7 +8,7 @@ class LoginUI extends StatefulWidget {
 }
 
 class _LoginUIState extends State<LoginUI> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   final TextEditingController _textEmailController = TextEditingController();
   final TextEditingController _textPasswordController = TextEditingController();
@@ -29,112 +28,108 @@ class _LoginUIState extends State<LoginUI> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<bool>(
-      stream: bloc.isAuthenticated,
-      builder: (context, authSnapshot) {
-        if (authSnapshot.hasData && authSnapshot.data) {
-          return HomePageUI();
-        } else {
+    return Scaffold(
+      key: _scaffoldKey,
+      resizeToAvoidBottomPadding: true,
+      body: StreamBuilder<bool>(
+        stream: bloc.isAuthenticated,
+        builder: (_, authSnapshot) {
+          if (authSnapshot.hasData && authSnapshot.data) {
+            Future.delayed(Duration.zero, () {
+              Navigator.of(context).pushReplacementNamed('/home');
+            });
+          }
+
           isAuthenticated = (authSnapshot.hasData && !authSnapshot.data);
           Future.delayed(Duration(milliseconds: 100), _showSnackBar);
 
-          return Scaffold(
-            key: _scaffoldKey,
-            resizeToAvoidBottomPadding: true,
-            appBar: AppBar(
-              title: Text("Login with BLoC"),
-              centerTitle: true,
-            ),
-            body: Stack(
-              children: <Widget>[
-                Container(
-                  alignment: Alignment.center,
-                  height: MediaQuery.of(context).size.height,
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    //shrinkWrap: true,
-                    children: <Widget>[
-                      StreamBuilder<String>(
-                        stream: bloc.email,
-                        builder: (context, emailSnapshot) => CustomTextField(
-                              label: "Email",
-                              textController: _textEmailController,
-                              hint: "Enter email",
-                              isRequired: true,
-                              requiredMessage: "The email is required",
-                              onChange: bloc.changeEmail,
-                              inputType: TextInputType.emailAddress,
-                              errorText: emailSnapshot.error,
-                            ),
+          return Stack(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height,
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    StreamBuilder<String>(
+                      stream: bloc.email,
+                      builder: (context, emailSnapshot) => CustomTextField(
+                        label: 'Email',
+                        textController: _textEmailController,
+                        hint: 'Enter email',
+                        isRequired: true,
+                        requiredMessage: 'The email is required',
+                        onChange: bloc.changeEmail,
+                        inputType: TextInputType.emailAddress,
+                        errorText: emailSnapshot.error,
                       ),
-                      SizedBox(height: 20),
-                      StreamBuilder<String>(
-                        stream: bloc.password,
-                        builder: (context, passwordSnapshot) =>
-                            CustomTextField(
-                              label: "Password",
-                              hint: "Enter password",
-                              isRequired: true,
-                              requiredMessage: "The password is required",
-                              onChange: bloc.changePassword,
-                              errorText: passwordSnapshot.error,
-                              hasPassword: true,
-                              textController: _textPasswordController,
-                            ),
+                    ),
+                    SizedBox(height: 20),
+                    StreamBuilder<String>(
+                      stream: bloc.password,
+                      builder: (context, passwordSnapshot) => CustomTextField(
+                        label: 'Password',
+                        hint: 'Enter password',
+                        isRequired: true,
+                        requiredMessage: 'The password is required',
+                        onChange: bloc.changePassword,
+                        errorText: passwordSnapshot.error,
+                        hasPassword: true,
+                        textController: _textPasswordController,
                       ),
-                      SizedBox(height: 20),
-                      StreamBuilder<bool>(
-                        stream: bloc.isValidData,
-                        builder: (context, snapshot) => Row(
-                              children: <Widget>[
-                                Expanded(
-                                  flex: 1,
-                                  child: RaisedButton(
-                                    color: Colors.tealAccent,
-                                    child: Text("Submit"),
-                                    onPressed: snapshot.hasData
-                                        ? () => bloc.authenticate()
-                                        : null,
-                                  ),
-                                ),
-                              ],
+                    ),
+                    SizedBox(height: 20),
+                    StreamBuilder<bool>(
+                      stream: bloc.isValidData,
+                      builder: (context, snapshot) => Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: RaisedButton(
+                              color: Colors.tealAccent,
+                              child: Text('Submit'),
+                              onPressed: snapshot.hasData
+                                  ? () => bloc.authenticate()
+                                  : null,
                             ),
-                      ),
-                    ],
-                  ),
-                ),
-                StreamBuilder<bool>(
-                  stream: bloc.isLoading,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData && snapshot.data) {
-                      return Container(
-                        height: MediaQuery.of(context).size.height,
-                        color: Colors.grey.withOpacity(.7),
-                        child: Center(
-                          child: CircularProgressIndicator(
-                            backgroundColor: Colors.green,
                           ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              StreamBuilder<bool>(
+                stream: bloc.isLoading,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data) {
+                    return Container(
+                      height: MediaQuery.of(context).size.height,
+                      color: Colors.grey.withOpacity(.7),
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.green,
                         ),
-                      );
-                    } else {
-                      return Container();
-                    }
-                  },
-                )
-              ],
-            ),
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              )
+            ],
           );
-        }
-      },
+        },
+      ),
     );
   }
 
   void _showSnackBar() {
     if (isAuthenticated) {
       _scaffoldKey.currentState.showSnackBar(SnackBar(
-        content: Text("User or password incorrect."),
+        content: Text('User or password incorrect.'),
         duration: Duration(seconds: 3),
       ));
     }
