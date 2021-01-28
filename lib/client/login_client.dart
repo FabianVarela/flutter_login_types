3f259e407ec1584e1751273c24620d4571d46bc1
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-01-27 22:49:51 -0500

    Update login client file

--- lib/client/login_client.dart
+++ lib/client/login_client.dart
@@ -2,10 +2,10 @@ class LoginClient {
   Future<String> authenticate(String username, String password) async {
     await Future.delayed(Duration(seconds: 3));
 
-    if (username == "prueba@prueba.com" && password == "password") {
-      return "MiToken";
+    if (username == 'prueba@prueba.com' && password == 'password') {
+      return 'MiToken';
     } else {
-      return "Error en autenticarse";
+      return 'Error en autenticarse';
     }
   }
 }
