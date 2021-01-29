f6547715add4ef4312bcf41149d1dbfcb6451f15
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-01-28 20:47:15 -0500

    Create method to validate number and phone

--- lib/common/validator.dart
+++ lib/common/validator.dart
@@ -18,13 +18,26 @@ mixin Validator {
       sink.addError('Password must be at least 4 characters');
     }
   });
+
+  final validateNumber = StreamTransformer<String, String>.fromHandlers(
+      handleData: (number, sink) {
+    if (number.isNotEmpty &&
+        _validateNumber(number) &&
+        _validatePhoneNumber(number)) {
+      sink.add(number);
+    } else {
+      sink.addError('Enter a valid number');
+    }
+  });
 }
 
-bool _validateEmail(String value) {
-  var regex =
-      "^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*\$";
+bool _validateEmail(String value) => _isMatch(
+    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+    value);
 
-  final regExp = RegExp(regex);
+bool _validateNumber(String value) => _isMatch(r'^[0-9]+$', value);
 
-  return regExp.hasMatch(value);
-}
+bool _validatePhoneNumber(String value) => _isMatch(r'^([0-9]){8,12}$', value);
+
+bool _isMatch(String regex, String value, [bool caseSensitive = true]) =>
+    RegExp(regex, caseSensitive: caseSensitive).hasMatch(value);
