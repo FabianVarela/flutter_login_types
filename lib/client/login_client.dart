0269ab09029cdb38268805d3f2a6b4bfa0a31863
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-01-28 20:46:58 -0500

    Add method to verify phone and code

--- lib/client/login_client.dart
+++ lib/client/login_client.dart
@@ -8,4 +8,24 @@ class LoginClient {
       return 'Error en autenticarse';
     }
   }
+
+  Future<String> verifyPhone(String phoneNumber) async {
+    await Future.delayed(Duration(seconds: 3));
+
+    if (phoneNumber == '3004567890') {
+      return '';
+    } else {
+      return 'Error en verificar';
+    }
+  }
+
+  Future<String> verifyCode(String code) async {
+    await Future.delayed(Duration(seconds: 3));
+
+    if (code == '0000') {
+      return 'MiToken';
+    } else {
+      return 'Error en autenticarse';
+    }
+  }
 }
--- lib/repository/login_repository.dart
+++ lib/repository/login_repository.dart
@@ -1,8 +1,12 @@
 import 'package:login_bloc/client/login_client.dart';
 
 class LoginRepository {
-  final client = LoginClient();
+  final _client = LoginClient();
 
   Future<String> authenticate(String username, String password) =>
-      client.authenticate(username, password);
+      _client.authenticate(username, password);
+
+  Future<String> verifyPhone(String phone) => _client.verifyPhone(phone);
+
+  Future<String> verifyCode(String code) => _client.verifyCode(code);
 }
