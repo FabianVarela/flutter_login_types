048a0226a98466a686f885c7e3736f3d9d92f446
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-01-30 20:35:11 -0500

    Change Activity class in android

--- android/app/src/main/kotlin/com/developer/fabian/login_bloc/MainActivity.kt
+++ android/app/src/main/kotlin/com/developer/fabian/login_bloc/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.developer.fabian.login_bloc
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
