a295ca9727dd831c305c7615f72d587eb4d1a092
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-02-02 22:27:51 -0500

    Call notification service on main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:login_bloc/common/notification_service.dart';
 import 'package:login_bloc/ui/login_biometric.ui.dart';
 import 'package:login_bloc/ui/login_passcode.dart';
 import 'package:login_bloc/ui/login_user_pass.ui.dart';
@@ -10,7 +11,18 @@ import 'ui/login_user_pass.ui.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    NotificationService.getInstance().init();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
