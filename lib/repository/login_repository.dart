a8e42592b13ce919e441dd5ef8c7f47e374dab12
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-07-29 13:07:49 -0500

    Update login_repository.dart methods

--- lib/repository/login_repository.dart
+++ lib/repository/login_repository.dart
@@ -3,12 +3,12 @@ import 'package:login_bloc/client/login_client.dart';
 class LoginRepository {
   final _client = LoginClient();
 
-  Future<String> authenticate(String username, String password) =>
+  Future<String?> authenticate(String username, String password) =>
       _client.authenticate(username, password);
 
-  Future<String> verifyPhone(String phone) => _client.verifyPhone(phone);
+  Future<String?> verifyPhone(String phone) => _client.verifyPhone(phone);
 
-  Future<String> verifyCode(String code) => _client.verifyCode(code);
+  Future<String?> verifyCode(String code) => _client.verifyCode(code);
 
   Future<Map<String, dynamic>> authenticateFacebook() =>
       _client.authenticateFacebook();
