625a0cbed2a0a1210db8acb1f808fbcd670c3b1f
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-01 14:49:20 -0500

    Update home.ui.dart adding dropdown to change language

--- lib/ui/home.ui.dart
+++ lib/ui/home.ui.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:login_bloc/bloc/language_bloc.dart';
 
 class HomePageUI extends StatefulWidget {
   const HomePageUI({Key? key}) : super(key: key);
@@ -43,9 +44,45 @@ class _HomePageUIState extends State<HomePageUI> with WidgetsBindingObserver {
         title: Text(localizations.homeTitle),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(localizations.homeText),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(localizations.homeText),
+            const SizedBox(height: 30),
+            StreamBuilder<Locale?>(
+              stream: languageBloc.localeStream,
+              builder: (_, snapshot) {
+                return DropdownButton<Locale>(
+                  hint: Text(localizations.changeLanguageTitle),
+                  value: snapshot.data,
+                  onChanged: (locale) =>
+                      languageBloc.setLanguage(locale!.languageCode),
+                  items: AppLocalizations.supportedLocales
+                      .map((locale) => DropdownMenuItem(
+                            value: locale,
+                            child: Text(_getText(locale.languageCode)),
+                          ))
+                      .toList(),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  String _getText(String lang) {
+    final localizations = AppLocalizations.of(context)!;
+
+    switch (lang) {
+      case 'es':
+        return localizations.spanishLanguage;
+      case 'en':
+      default:
+        return localizations.englishLanguage;
+    }
   }
 }
