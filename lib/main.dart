da2fe1e0feaa8071aa40309ae3187e3b39fc2b23
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-01 14:50:39 -0500

    Update main.dart adding stream to change language automatically

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:login_bloc/bloc/language_bloc.dart';
 import 'package:login_bloc/common/notification_service.dart';
 import 'package:login_bloc/common/routes.dart';
 import 'package:login_bloc/ui/login_biometric.ui.dart';
@@ -24,40 +25,54 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+
     NotificationService.getInstance().init();
+    languageBloc.getLanguage();
+  }
+
+  @override
+  void dispose() {
+    languageBloc.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        textTheme: GoogleFonts.notoSansTextTheme(Theme.of(context).textTheme),
-      ),
-      // locale: null,
-      // locale: const Locale('en'),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      initialRoute: Routes.signInOptions,
-      localeResolutionCallback: (locale, supportedLocales) {
-        print('Locale: $locale · Locales: $supportedLocales');
-        if (locale == null) return supportedLocales.first;
+    return StreamBuilder<Locale?>(
+      stream: languageBloc.localeStream,
+      builder: (_, snapshot) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.teal,
+            textTheme: GoogleFonts.notoSansTextTheme(
+              Theme.of(context).textTheme,
+            ),
+          ),
+          locale: snapshot.data,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          initialRoute: Routes.signInOptions,
+          localeResolutionCallback: (locale, supportedLocales) {
+            print('Locale: $locale · Locales: $supportedLocales');
+            if (locale == null) return supportedLocales.first;
 
-        for (final currentLocale in supportedLocales) {
-          final isLanguage = currentLocale.languageCode == locale.languageCode;
-          final isCountry = currentLocale.countryCode == locale.countryCode;
+            for (final currentLocale in supportedLocales) {
+              final isLang = currentLocale.languageCode == locale.languageCode;
+              // currentLocale.countryCode == locale.countryCode;
 
-          if (isLanguage || isCountry) return currentLocale;
-        }
-        return supportedLocales.first;
-      },
-      onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
-      routes: {
-        Routes.signInOptions: (_) => const SignInOptionsUI(),
-        Routes.signInUserPass: (_) => const LoginUI(),
-        Routes.signInPasscode: (_) => const LoginPasscodeUI(),
-        Routes.signInBiometric: (_) => const LoginBiometric(),
-        Routes.home: (_) => const HomePageUI(),
+              if (isLang) return currentLocale;
+            }
+            return supportedLocales.first;
+          },
+          onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
+          routes: {
+            Routes.signInOptions: (_) => const SignInOptionsUI(),
+            Routes.signInUserPass: (_) => const LoginUI(),
+            Routes.signInPasscode: (_) => const LoginPasscodeUI(),
+            Routes.signInBiometric: (_) => const LoginBiometric(),
+            Routes.home: (_) => const HomePageUI(),
+          },
+        );
       },
     );
   }
