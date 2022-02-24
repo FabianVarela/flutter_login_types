cfb6ca9ec4b305a619fcb35c4aa11856688d2896
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-02-24 17:36:29 -0500

    Add and update Android files

--- android/app/src/main/kotlin/com/developer/fabian/login_bloc/MainActivity.kt
+++ android/app/src/main/kotlin/com/developer/fabian/login_bloc/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.developer.fabian.login_bloc
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
