3615cf7777161ef828fc2b6e914e57eaba70f63c
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-03-02 10:47:27 -0500

    Change screen by hook widget

--- lib/ui/login_user_pass.ui.dart
+++ lib/ui/login_user_pass.ui.dart
@@ -13,82 +13,70 @@ import 'package:login_bloc/ui/widgets/custom_button.dart';
 import 'package:login_bloc/ui/widgets/custom_textfield.dart';
 import 'package:login_bloc/ui/widgets/loading.dart';
 
-class LoginUI extends StatefulWidget {
+class LoginUI extends HookWidget {
   const LoginUI({Key? key}) : super(key: key);
 
   @override
-  _LoginUIState createState() => _LoginUIState();
-}
-
-class _LoginUIState extends State<LoginUI> {
-  @override
-  void dispose() {
-    loginBloc.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      backgroundColor: CustomColors.white,
-      body: Stack(
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                height: MediaQuery.of(context).size.height,
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    TextFieldEmail(bloc: loginBloc),
-                    const SizedBox(height: 20),
-                    TextFieldPassword(bloc: loginBloc),
-                    const SizedBox(height: 20),
-                    SubmitButton(
-                      bloc: loginBloc,
-                      onSendMessage: _showSnackBar,
-                      onGoToScreen: _goToHomeScreen,
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 30,
-                left: 16,
-                child: IconButton(
-                  icon: const Icon(Icons.arrow_back_ios),
-                  onPressed: () => Navigator.of(context).pop(),
-                ),
-              ),
-            ],
+    useEffect(
+      () {
+        return loginBloc.dispose;
+      },
+      const [],
+    );
+
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            foregroundColor: Colors.black,
           ),
-          StreamBuilder<bool>(
-            initialData: false,
-            stream: loginBloc.isLoading,
-            builder: (_, loadSnapshot) => Offstage(
-              offstage: !loadSnapshot.data!,
-              child: const Loading(),
+          extendBodyBehindAppBar: true,
+          resizeToAvoidBottomInset: true,
+          backgroundColor: CustomColors.white,
+          body: Container(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _TextFieldEmail(bloc: loginBloc),
+                const SizedBox(height: 20),
+                _TextFieldPassword(bloc: loginBloc),
+                const SizedBox(height: 20),
+                _SubmitButton(
+                  bloc: loginBloc,
+                  onSendMessage: (value) => _showSnackBar(context, value),
+                  onGoToScreen: () => _goToHomeScreen(context),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+        StreamBuilder<bool>(
+          initialData: false,
+          stream: loginBloc.isLoading,
+          builder: (_, loadSnapshot) => Offstage(
+            offstage: !loadSnapshot.data!,
+            child: const Loading(),
+          ),
+        ),
+      ],
     );
   }
 
-  void _showSnackBar(String message) => Future.delayed(
+  void _showSnackBar(BuildContext context, String message) => Future.delayed(
         const Duration(milliseconds: 100),
         () => MessageService.getInstance().showMessage(context, message),
       );
 
-  Future<void> _goToHomeScreen() => Navigator.of(context)
+  Future<void> _goToHomeScreen(BuildContext context) => Navigator.of(context)
       .pushNamedAndRemoveUntil(Routes.home, (Route<dynamic> route) => false);
 }
 
-class TextFieldEmail extends HookWidget {
-  const TextFieldEmail({Key? key, required this.bloc}) : super(key: key);
+class _TextFieldEmail extends HookWidget {
+  const _TextFieldEmail({Key? key, required this.bloc}) : super(key: key);
 
   final LoginBloc bloc;
 
@@ -123,8 +111,8 @@ class TextFieldEmail extends HookWidget {
   }
 }
 
-class TextFieldPassword extends HookWidget {
-  const TextFieldPassword({Key? key, required this.bloc}) : super(key: key);
+class _TextFieldPassword extends HookWidget {
+  const _TextFieldPassword({Key? key, required this.bloc}) : super(key: key);
 
   final LoginBloc bloc;
 
@@ -158,8 +146,8 @@ class TextFieldPassword extends HookWidget {
   }
 }
 
-class SubmitButton extends StatelessWidget {
-  const SubmitButton({
+class _SubmitButton extends StatelessWidget {
+  const _SubmitButton({
     Key? key,
     required this.bloc,
     required this.onSendMessage,
