b75bd57767f5d1e7ed71838a27bbdfce57e66074
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-03-02 19:18:55 -0500

    Add firebase ui auth in screen

--- lib/ui/firebase_auth.ui.dart
+++ lib/ui/firebase_auth.ui.dart
@@ -1,25 +1,64 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:login_bloc/l10n/l10n.dart';
 
 class FirebaseAuthUI extends StatelessWidget {
   const FirebaseAuthUI({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    const clientId = String.fromEnvironment('CLIENT_ID');
+    final localization = context.localizations;
+
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          const Center(
-            child: Text('Firebase Auth'),
-          ),
-          Positioned(
-            top: 30,
-            left: 16,
-            child: IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+      ),
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (_, snapshot) {
+          if (!snapshot.hasData) {
+            return SignInScreen(
+              headerBuilder: (_, __, ___) => Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.network(
+                    'https://firebase.flutter.dev/img/flutterfire_300x.png',
+                  ),
+                ),
+              ),
+              subtitleBuilder: (_, action) => Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  action == AuthAction.signIn
+                      ? localization.firebaseSubtitleSignIn
+                      : localization.firebaseSubtitleSignUp,
+                ),
+              ),
+              providerConfigs: const [
+                EmailProviderConfiguration(),
+                GoogleProviderConfiguration(clientId: clientId),
+                FacebookProviderConfiguration(clientId: clientId),
+                AppleProviderConfiguration(),
+              ],
+              footerBuilder: (_, action) => Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  action == AuthAction.signIn
+                      ? localization.firebaseFooterSignIn
+                      : localization.firebaseFooterSignUp,
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ),
+            );
+          }
+
+          return const ProfileScreen();
+        },
       ),
     );
   }
