217430c0495ad06272a0b3e466d363d2e4c474ab
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-11-04 13:20:26 -0500

    Update main.dart and bootstrap.dart files

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,11 +2,16 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
+
+  await SystemChrome.setPreferredOrientations(
+    <DeviceOrientation>[DeviceOrientation.portraitUp],
+  );
 
   await runZonedGuarded(
     () async => runApp(await builder()),
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:login_bloc/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await bootstrap(() => const LoginApp());
 }
