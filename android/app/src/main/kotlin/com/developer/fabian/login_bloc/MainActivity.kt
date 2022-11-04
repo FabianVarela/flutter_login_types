62b8c40be14a336b108e5f805505bea4b6dfac59
FabianVarela <jfabian.303@gmail.com>
2022-11-04 15:13:25 -0500

    Update bundle in Android platform

--- android/app/src/main/kotlin/com/developer/fabian/login_bloc/MainActivity.kt
+++ android/app/src/main/kotlin/com/developer/fabian/login_bloc/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.developer.fabian.login_bloc
+package com.developer.fabian.flutter_login_types
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
