9a43fe4ce2156a3d1c606a5a1a615a7639ad1c49
FabianVarela <jfabian.303@gmail.com>
2022-11-07 19:34:44 -0500

    Create dependencies for login client and repository

--- lib/core/dependencies/dependencies.dart
+++ lib/core/dependencies/dependencies.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login_types/client/login_client.dart';
 import 'package:flutter_login_types/client/preferences.dart';
 import 'package:flutter_login_types/core/notifiers/language_notifier.dart';
 import 'package:flutter_login_types/repository/language_repository.dart';
+import 'package:flutter_login_types/repository/login_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,8 +15,16 @@ final preferencesProvider = Provider<Preferences>((_) {
   throw UnimplementedError('You must implement Preferences provider');
 });
 
+final loginClientProvider = Provider<LoginClient>((_) {
+  throw UnimplementedError('You must implement LoginClient provider');
+});
+
 final languageRepositoryProvider = Provider<LanguageRepository>((_) {
   throw UnimplementedError('You must implement LanguageRepository provider');
+});
+
+final loginRepositoryProvider = Provider<LoginRepository>((_) {
+  throw UnimplementedError('You must implement LoginRepository provider');
 });
 
 final languageNotifierProvider =
