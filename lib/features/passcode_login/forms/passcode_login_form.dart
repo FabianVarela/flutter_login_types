eb49b959214644a23fadd4d1243bbbbe5721697d
FabianVarela <jfabian.303@gmail.com>
2022-11-08 15:18:43 -0500

    Create class to handle form inputs and validations

---
+++ lib/features/passcode_login/forms/passcode_login_form.dart
@@ -0,0 +1,32 @@
+import 'package:formz/formz.dart';
+
+enum PhoneInputValidation { invalid, empty }
+
+class PhoneInputText extends FormzInput<String, PhoneInputValidation> {
+  const PhoneInputText.pure() : super.pure('');
+
+  const PhoneInputText.dirty([super.value = '']) : super.dirty();
+
+  @override
+  PhoneInputValidation? validator(String value) {
+    if (value.isEmpty) return PhoneInputValidation.empty;
+
+    final regex = RegExp(r'^([0-9]){8,12}$');
+    if (!regex.hasMatch(value)) return PhoneInputValidation.invalid;
+
+    return null;
+  }
+}
+
+class PasscodeLoginForm with FormzMixin {
+  const PasscodeLoginForm({this.phoneInput = const PhoneInputText.pure()});
+
+  final PhoneInputText phoneInput;
+
+  PasscodeLoginForm copyWith({PhoneInputText? phoneInput}) {
+    return PasscodeLoginForm(phoneInput: phoneInput ?? this.phoneInput);
+  }
+
+  @override
+  List<FormzInput<dynamic, dynamic>> get inputs => [phoneInput];
+}
---
+++ lib/features/passcode_login/forms/passcode_login_form_notifier.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_login_types/features/passcode_login/forms/passcode_login_form.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class PasscodeLoginFormNotifier extends StateNotifier<PasscodeLoginForm> {
+  PasscodeLoginFormNotifier([super.state = const PasscodeLoginForm()]);
+
+  void onChangePhone(String value) {
+    state = state.copyWith(phoneInput: PhoneInputText.dirty(value));
+  }
+}
+
+final passcodeFormNotifierProvider =
+    StateNotifierProvider<PasscodeLoginFormNotifier, PasscodeLoginForm>(
+  (ref) => PasscodeLoginFormNotifier(),
+);
