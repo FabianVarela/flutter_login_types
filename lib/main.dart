b8a0519d45c7f5f04be608a0b0c5a085b7785330
FabianVarela <jfabian.303@gmail.com>
2022-11-08 23:12:12 -0500

    Remove Firebase initialize in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login_types/app.dart';
 import 'package:flutter_login_types/bootstrap.dart';
-import 'package:flutter_login_types/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await bootstrap(appProvider: configureAppProvider(), () => const LoginApp());
 }
