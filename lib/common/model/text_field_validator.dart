bde13da13679e735327b1c82a0c069486fbb603e
FabianVarela <jfabian.303@gmail.com>
2022-11-08 23:11:29 -0500

    Remove unused files and classes

--- lib/common/model/text_field_validator.dart
+++
@@ -1,8 +0,0 @@
-enum TextValidator { empty, text, email, password, numeric }
-
-class TextFieldValidator {
-  TextFieldValidator({this.text, this.validator});
-
-  final String? text;
-  final TextValidator? validator;
-}
