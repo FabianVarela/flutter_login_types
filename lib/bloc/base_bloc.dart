abb7726361c2513dd703b401172c4571cdbffccf
FabianVarela <jfabian.303@gmail.com>
2022-11-08 23:11:05 -0500

    Remove old facebook bloc and base bloc

--- lib/bloc/base_bloc.dart
+++
@@ -1,9 +0,0 @@
-import 'package:rxdart/rxdart.dart';
-
-abstract class BaseBloc {
-  final loading = PublishSubject<bool>();
-
-  Stream<bool> get isLoading => loading.stream;
-
-  void dispose();
-}
