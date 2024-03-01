209ccb7f56d855475a8199b33d8a3d742d0526bb
FabianVarela <jfabian.303@gmail.com>
2024-03-01 14:37:37 -0500

    Remove unused files

--- lib/features/simple_login/dependency.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_login_types/core/dependencies/dependencies.dart';
-import 'package:flutter_login_types/features/simple_login/notifier/simple_login_notifier.dart';
-import 'package:flutter_login_types/features/simple_login/notifier/simple_login_state.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final simpleLoginNotifierProvider =
-    StateNotifierProvider.autoDispose<SimpleLoginNotifier, SimpleLoginState>(
-  (ref) => SimpleLoginNotifier(ref.watch(loginRepositoryProvider)),
-);
