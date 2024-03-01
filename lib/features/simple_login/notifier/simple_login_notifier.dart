93d59f9f40aea2f2e3297af8be100eacf233b0d0
FabianVarela <jfabian.303@gmail.com>
2024-03-01 14:37:19 -0500

    Become to AsyncNotifier and simplify state

--- lib/features/simple_login/notifier/simple_login_notifier.dart
+++ lib/features/simple_login/notifier/simple_login_notifier.dart
@@ -1,21 +1,25 @@
-import 'package:flutter_login_types/core/repository/login_repository.dart';
-import 'package:flutter_login_types/features/simple_login/notifier/simple_login_state.dart';
+import 'dart:async';
+
+import 'package:flutter_login_types/core/dependencies/dependencies.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SimpleLoginNotifier extends StateNotifier<SimpleLoginState> {
-  SimpleLoginNotifier(
-    this._repository, [
-    super.state = const SimpleLoginState.initial(),
-  ]);
-
-  final LoginRepository _repository;
+class SimpleLoginNotifier extends AutoDisposeAsyncNotifier<bool?> {
+  @override
+  FutureOr<bool?> build() => null;
 
   Future<void> authenticate(String email, String password) async {
-    state = const SimpleLoginState.loading();
-    final token = await _repository.authenticate(email, password);
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      final repository = ref.read(loginRepositoryProvider);
+      final token = await repository.authenticate(email, password);
 
-    state = token != null && token == 'MiToken'
-        ? const SimpleLoginState.success()
-        : const SimpleLoginState.error();
+      if (token != null && token == 'MiToken') return true;
+      throw Exception();
+    });
   }
 }
+
+final simpleLoginNotifierProvider =
+    AsyncNotifierProvider.autoDispose<SimpleLoginNotifier, bool?>(
+  SimpleLoginNotifier.new,
+);
