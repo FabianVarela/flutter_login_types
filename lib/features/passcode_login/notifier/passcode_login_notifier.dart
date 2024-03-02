60fbd86a865c198ad559fc175b2dd69645f003f2
FabianVarela <jfabian.303@gmail.com>
2024-03-02 12:20:52 -0500

    Become to AsyncNotifier and change state value

--- lib/features/passcode_login/notifier/passcode_login_notifier.dart
+++ lib/features/passcode_login/notifier/passcode_login_notifier.dart
@@ -1,28 +1,40 @@
-import 'package:flutter_login_types/core/repository/login_repository.dart';
-import 'package:flutter_login_types/features/passcode_login/notifier/passcode_login_state.dart';
+import 'dart:async';
+
+import 'package:flutter_login_types/core/dependencies/dependencies.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class PasscodeLoginNotifier extends StateNotifier<PasscodeLoginState> {
-  PasscodeLoginNotifier(
-    this._repository, [
-    super.state = const PasscodeLoginState.initial(),
-  ]);
+enum PasscodeLogin { none, phone, passcode }
 
-  final LoginRepository _repository;
+class PasscodeLoginNotifier extends AutoDisposeAsyncNotifier<PasscodeLogin> {
+  @override
+  FutureOr<PasscodeLogin> build() => PasscodeLogin.none;
+
+  void restore() => state = const AsyncValue.data(PasscodeLogin.none);
 
   Future<void> verifyPhone(String phoneNumber) async {
-    state = const PasscodeLoginState.loading();
-    final token = await _repository.verifyPhone(phoneNumber);
-    state = token != null && token.isEmpty
-        ? const PasscodeLoginState.success(phase: PasscodeLoginPhase.phone)
-        : const PasscodeLoginState.error(phase: PasscodeLoginPhase.phone);
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      final repository = ref.read(loginRepositoryProvider);
+      final token = await repository.verifyPhone(phoneNumber);
+
+      if (token != null && token.isEmpty) return PasscodeLogin.phone;
+      throw Exception();
+    });
   }
 
   Future<void> verifyCode(String passcode) async {
-    state = const PasscodeLoginState.loading();
-    final token = await _repository.verifyCode(passcode);
-    state = token != null && token == 'MiToken'
-        ? const PasscodeLoginState.success(phase: PasscodeLoginPhase.passcode)
-        : const PasscodeLoginState.error(phase: PasscodeLoginPhase.passcode);
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      final repository = ref.read(loginRepositoryProvider);
+      final token = await repository.verifyCode(passcode);
+
+      if (token != null && token == 'MiToken') return PasscodeLogin.passcode;
+      throw Exception();
+    });
   }
 }
+
+final passcodeLoginNotifierProvider =
+    AsyncNotifierProvider.autoDispose<PasscodeLoginNotifier, PasscodeLogin>(
+  PasscodeLoginNotifier.new,
+);
