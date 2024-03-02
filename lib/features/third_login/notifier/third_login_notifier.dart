03eb6b2ef72912c49e74e8ae910ab1efda137b96
FabianVarela <jfabian.303@gmail.com>
2024-03-02 13:37:44 -0500

    Become to AsyncNotifier

--- lib/features/third_login/notifier/third_login_notifier.dart
+++ lib/features/third_login/notifier/third_login_notifier.dart
@@ -1,83 +1,93 @@
+import 'dart:async';
+
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_login_types/core/dependencies/dependencies.dart';
-import 'package:flutter_login_types/core/repository/login_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:twitter_login/twitter_login.dart';
 
-enum ThirdLoginResult { none, loading, progress, success, cancelled, error }
+enum ThirdLoginResult { none, progress, success, cancelled, error }
 
-class ThirdLoginNotifier extends StateNotifier<ThirdLoginResult> {
-  ThirdLoginNotifier(this._repository, [super.state = ThirdLoginResult.none]);
-
-  final LoginRepository _repository;
+class ThirdLoginNotifier extends AutoDisposeAsyncNotifier<ThirdLoginResult> {
+  @override
+  FutureOr<ThirdLoginResult> build() => ThirdLoginResult.none;
 
   Future<void> authenticateGoogle() async {
-    state = ThirdLoginResult.loading;
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      try {
+        final repository = ref.read(loginRepositoryProvider);
+        final googleResult = await repository.authenticateGoogle();
 
-    try {
-      final googleResult = await _repository.authenticateGoogle();
-      state = googleResult != null
-          ? ThirdLoginResult.success
-          : ThirdLoginResult.cancelled;
-    } on Exception catch (_) {
-      state = ThirdLoginResult.error;
-    }
+        return googleResult != null
+            ? ThirdLoginResult.success
+            : ThirdLoginResult.cancelled;
+      } on Exception catch (_) {
+        return ThirdLoginResult.error;
+      }
+    });
   }
 
   Future<void> authenticateApple() async {
-    state = ThirdLoginResult.loading;
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      try {
+        final repository = ref.read(loginRepositoryProvider);
+        final appleResult = await repository.authenticateApple();
 
-    try {
-      final appleResult = await _repository.authenticateApple();
-      state = appleResult != null
-          ? ThirdLoginResult.success
-          : ThirdLoginResult.error;
-    } on Exception catch (e) {
-      if (e is SignInWithAppleAuthorizationException) {
-        if (e.code == AuthorizationErrorCode.canceled) {
-          state = ThirdLoginResult.cancelled;
-          return;
+        return appleResult != null
+            ? ThirdLoginResult.success
+            : ThirdLoginResult.error;
+      } on Exception catch (e) {
+        if (e is SignInWithAppleAuthorizationException) {
+          if (e.code == AuthorizationErrorCode.canceled) {
+            return ThirdLoginResult.cancelled;
+          }
         }
+        return ThirdLoginResult.error;
       }
-
-      state = ThirdLoginResult.error;
-    }
+    });
   }
 
   Future<void> authenticateFacebook() async {
-    state = ThirdLoginResult.loading;
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      final repository = ref.read(loginRepositoryProvider);
+      final facebookResult = await repository.authenticateFacebook();
 
-    final facebookResult = await _repository.authenticateFacebook();
-    if (facebookResult.containsKey('status')) {
-      state = switch (facebookResult['status'] as LoginStatus) {
-        LoginStatus.success => ThirdLoginResult.success,
-        LoginStatus.cancelled => ThirdLoginResult.cancelled,
-        LoginStatus.failed => ThirdLoginResult.error,
-        LoginStatus.operationInProgress => ThirdLoginResult.progress,
-      };
-    } else {
-      state = ThirdLoginResult.error;
-    }
+      if (facebookResult.containsKey('status')) {
+        return switch (facebookResult['status'] as LoginStatus) {
+          LoginStatus.success => ThirdLoginResult.success,
+          LoginStatus.cancelled => ThirdLoginResult.cancelled,
+          LoginStatus.failed => ThirdLoginResult.error,
+          LoginStatus.operationInProgress => ThirdLoginResult.progress,
+        };
+      } else {
+        return ThirdLoginResult.error;
+      }
+    });
   }
 
   Future<void> authenticateTwitter() async {
-    try {
-      state = ThirdLoginResult.loading;
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() async {
+      try {
+        final repository = ref.read(loginRepositoryProvider);
+        final twitterResult = await repository.authenticateTwitter();
 
-      final twitterResult = await _repository.authenticateTwitter();
-      state = switch (twitterResult['status'] as TwitterLoginStatus) {
-        TwitterLoginStatus.loggedIn => ThirdLoginResult.success,
-        TwitterLoginStatus.cancelledByUser => ThirdLoginResult.cancelled,
-        TwitterLoginStatus.error => ThirdLoginResult.error,
-      };
-    } on Exception catch (_) {
-      state = ThirdLoginResult.error;
-    }
+        return switch (twitterResult['status'] as TwitterLoginStatus) {
+          TwitterLoginStatus.loggedIn => ThirdLoginResult.success,
+          TwitterLoginStatus.cancelledByUser => ThirdLoginResult.cancelled,
+          TwitterLoginStatus.error => ThirdLoginResult.error,
+        };
+      } on Exception catch (_) {
+        return ThirdLoginResult.error;
+      }
+    });
   }
 }
 
 final thirdLoginNotifierProvider =
-    StateNotifierProvider.autoDispose<ThirdLoginNotifier, ThirdLoginResult>(
-  (ref) => ThirdLoginNotifier(ref.watch(loginRepositoryProvider)),
+    AsyncNotifierProvider.autoDispose<ThirdLoginNotifier, ThirdLoginResult>(
+  ThirdLoginNotifier.new,
 );
