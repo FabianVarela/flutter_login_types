cf7bca10db3d2e2c718b0b3a77e7119df0e4dc11
FabianVarela <jfabian.303@gmail.com>
2024-03-02 22:02:09 -0500

    Update Android files setting new bundle id and update libraries versions

--- android/app/src/main/kotlin/com/developer/fabian/login_types/MainActivity.kt
+++ android/app/src/main/kotlin/com/developer/fabian/login_types/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.developer.fabian.flutter_login_types
+package com.developer.fabian.login_types
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
