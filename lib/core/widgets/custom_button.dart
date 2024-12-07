911b9573b440834991f8a91035a9a2d341998feb
FabianVarela <jfabian.303@gmail.com>
2024-12-06 20:11:52 -0500

    Replace with ElevatedButton.icon

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -22,44 +22,33 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return ElevatedButton.icon(
       style: ElevatedButton.styleFrom(
         backgroundColor: backgroundColor,
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
       onPressed: onPress,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          if (icon != null && direction == IconDirection.left)
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(right: 5),
-                child: icon,
-              ),
-            ),
-          Expanded(
-            flex: 3,
-            child: Text(
-              text,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                height: 1.2,
-                fontWeight: FontWeight.bold,
-                color: foregroundColor,
-              ),
-            ),
-          ),
-          if (icon != null && direction == IconDirection.right)
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: icon,
-              ),
-            ),
-        ],
+      iconAlignment: switch (direction) {
+        IconDirection.left => IconAlignment.start,
+        IconDirection.right => IconAlignment.end,
+      },
+      icon: Padding(
+        padding: switch (direction) {
+          IconDirection.left => const EdgeInsets.only(right: 5),
+          IconDirection.right => const EdgeInsets.only(left: 5),
+        },
+        child: icon,
+      ),
+      label: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 16,
+          height: 1.2,
+          fontWeight: FontWeight.bold,
+          color: foregroundColor,
+        ),
       ),
     );
   }
