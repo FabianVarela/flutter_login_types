47a6dc33dd714b103da5ccb3aaf04082b8f18645
FabianVarela <jfabian.303@gmail.com>
2024-12-10 11:03:05 -0500

    Inject secure storage package client and repository

--- lib/core/dependencies/dependencies.dart
+++ lib/core/dependencies/dependencies.dart
@@ -3,13 +3,23 @@ import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_login_types/core/client/login_client.dart';
 import 'package:flutter_login_types/core/client/preferences.dart';
+import 'package:flutter_login_types/core/client/secure_storage.dart';
 import 'package:flutter_login_types/core/config/app_config.dart';
 import 'package:flutter_login_types/core/repository/language_repository.dart';
 import 'package:flutter_login_types/core/repository/login_repository.dart';
+import 'package:flutter_login_types/core/repository/session_repository.dart';
 import 'package:flutter_login_types/core/services/notification_service.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+final flutterSecureStorageProvider = Provider<FlutterSecureStorage>((_) {
+  return const FlutterSecureStorage(
+    aOptions: AndroidOptions(encryptedSharedPreferences: true),
+    iOptions: IOSOptions(accessibility: KeychainAccessibility.first_unlock),
+  );
+});
 
 final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
   throw UnimplementedError('You must implement SharedPreferences provider');
@@ -17,6 +27,12 @@ final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
 
 final localAuthenticationProvider = Provider<LocalAuthentication>(
   (_) => LocalAuthentication(),
+);
+
+final secureStorageProvider = Provider<SecureStorage>(
+  (ref) => SecureStorage(
+    flutterSecureStorage: ref.watch(flutterSecureStorageProvider),
+  ),
 );
 
 final preferencesProvider = Provider<Preferences>(
@@ -47,6 +63,10 @@ final loginClientProvider = Provider<LoginClient>(
     auth0: ref.watch(auth0Provider),
   ),
 );
+
+final sessionRepositoryProvider = Provider<SessionRepository>((ref) {
+  return SessionRepository(secureStorage: ref.watch(secureStorageProvider));
+});
 
 final languageRepositoryProvider = Provider<LanguageRepository>(
   (ref) => LanguageRepository(preferences: ref.watch(preferencesProvider)),
