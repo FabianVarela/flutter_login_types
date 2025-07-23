cc0afd7d9dd7aa760b2f4a25c330e78793f719eb
FabianVarela <jfabian.303@gmail.com>
2025-07-22 18:28:41 -0600

    Improve arb and gen ignoring in repository

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_login_types/l10n/generated/app_localizations.dart';
+import 'package:flutter_login_types/l10n/gen/app_localizations.dart';
 
-export 'package:flutter_login_types/l10n/generated/app_localizations.dart';
+export 'package:flutter_login_types/l10n/gen/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
-  AppLocalizations get localizations => AppLocalizations.of(this)!;
+  AppLocalizations get localizations => AppLocalizations.of(this);
 }
