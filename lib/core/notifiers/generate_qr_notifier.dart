1c75ea5e5636874361f3010f63f90a99bd82dabd
FabianVarela <jfabian.303@gmail.com>
2026-05-10 15:35:32 -0500

    Create notifier to generate QR code

---
+++ lib/core/notifiers/generate_qr_notifier.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter_login_types/core/dependencies/dependencies.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+typedef QrData = ({String secret, String account});
+
+class GenerateQrNotifier extends AsyncNotifier<String> {
+  GenerateQrNotifier(this.data);
+
+  final QrData data;
+
+  static const _issuer = 'FlutterLoginTypes';
+
+  @override
+  FutureOr<String> build() async {
+    final totpCore = ref.watch(totpCoreServiceProvider);
+    return totpCore.generateQrUri(
+      qrInfo: (issuer: _issuer, secret: data.secret, accountName: data.account),
+    );
+  }
+}
+
+final generateQrProvider = AsyncNotifierProvider.autoDispose
+    .family<GenerateQrNotifier, String, QrData>(GenerateQrNotifier.new);
